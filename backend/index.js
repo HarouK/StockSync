6682f4e0ea373bcf29eeac189844548145bc7fb6
Adim2P <miguellicauco@gmail.com>
2026-05-13 01:03:15 +0800

    Setup: Backend Initialization and Schema for DB

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const db = require('./db/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('StockSync API Running');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
