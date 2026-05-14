8641af59cd7954e0e51caae4db86cc9dd3e875ae
HarouK <concepcionisaac4@gmail.com>
2026-05-14 17:06:55 +0800

    Add: Removed explore tab (I forgot lol)

--- stocksync-app/app/(tabs)/index.tsx
+++ stocksync-app/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import {View, Text, StyleSheet, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
+
 export default function HomeScreen() {
   return (
   <ScrollView style={styles.container}>
